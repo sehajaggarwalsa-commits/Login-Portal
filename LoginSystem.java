204d5b52810bb3cece0d7a50adf6c68ba269c721
Sehaj Aggarwal <sehajaggarwal.sa@gmail.com>
2026-04-14 16:47:51 -0400

    Add files via upload

---
+++ LoginSystem.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class LoginSystem {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+        String correctUsername = "Sehaj";
+        String correctPassword = "1234";
+
+        System.out.print("Enter username: ");
+        String username = sc.nextLine();
+
+        System.out.print("Enter password: ");
+        String password = sc.nextLine();
+
+        if (username.equals(correctUsername) && password.equals(correctPassword)) {
+            System.out.println("Login successful"); } else {
+            System.out.println("Invalid username or password");
+        } } }
